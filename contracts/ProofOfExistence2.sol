c5cacc78c525c81940d2f4e16cbf602dadaa75ef
shadowsyntax <tibetianwolf@gmail.com>
2018-08-20 00:36:11 +0100

    Rewrote PoE1 to PoE2

---
+++ contracts/ProofOfExistence2.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.15;
+
+// Proof of Existence contract, version 2
+contract ProofOfExistence2 {
+    // State
+    bytes32[] private proofs;
+
+    // Store a proof of existence in the contract state
+    // *transactional function*
+    function storeProof (bytes proof) public {
+        proofs.push(proof);
+    }
+
+    // Calculate and store the proof for a document
+    // *transactional function*
+    function notarize (string document) public {
+        bytes32 proof = proofFor(document);
+        storeProof(proof);
+    }
+
+    // Helper function to get a document's sha256
+    // *read-only function*
+    function proofFor(string document) view returns (bytes32) {
+        return sha256(document);
+    }
+
+    // Check if a document has been notarized
+    // *read-only function*
+    function checkDocument(string document) view returns (bool) {
+        bytes32 proof = proofFor(document);
+        return hasProof(proof);
+    }
+
+    // Returns true if proof is stored
+    // *read-only function*
+    function hasProof(bytes32 proof) view returns (bool) {
+        for (uint256 i =0; i < proofs.length; i++) {
+            if (proofs[i] == proof) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
